f7c0156aff4382a913bc1c8c9e48ec5e7802875d
Alexander Vasin <alexander.v.vasin@gmail.com>
2014-11-10 14:53:21 +0400

    Added sorting and generation of current date

--- generator.py
+++ generator.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
-import os, sys
+import os, sys, time
+
+filelist = []
 
 print """= Good Songs to Play
-Alexander Vasin <alexander.v.vasin@gmail.com>
-07.11.2014
-:toc:
+Alexander Vasin <alexander.v.vasin@gmail.com>"""
+print time.strftime("%d/%m/%Y")
+print """:toc:
 """
 
 for roots, dirs,files in os.walk('.'):
     for afile in files:
         filepath = os.path.join(roots, afile)
         if filepath.find('.ad') > 0 and roots <> '.':
-            tfile = open(filepath)
-            for lines in tfile:
-                sys.stdout.write(lines)
-            print
+            filelist.append(filepath)
+
+filelist.sort()    
+for item in filelist:
+    tfile = open(item)
+    for lines in tfile:
+        sys.stdout.write(lines)
+    print "\n"
