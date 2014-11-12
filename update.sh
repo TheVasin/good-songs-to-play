8564435f84e88b8293b089ac25e5d40f5a30a667
Alexander Vasin <alexander.v.vasin@gmail.com>
2014-11-12 17:33:30 +0400

    Fixed update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ git rm index.html
 git commit -m "index.html removed by script"
 git checkout master
 ./publish.sh
+git commit -a -m "Updated by script"
 git checkout gh-pages
 git add index.html
 git commit index.html -m "index.html added by script"
