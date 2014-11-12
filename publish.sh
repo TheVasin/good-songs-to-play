bd78829866410d0c033e8ee37fc9001d1aae8839
Alexander Vasin <alexander.v.vasin@gmail.com>
2014-11-12 15:55:30 +0400

    Initiated stylesheet

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./generator.py > Good-songs-to-play.ad
-asciidoctor -v -a stylesheet! Good-songs-to-play.ad -o index.html
+asciidoctor -v -a stylesheet=stylesheets/minimal.css Good-songs-to-play.ad -o index.html
