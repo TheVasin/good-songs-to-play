8212285006497056d7b851519a507456a4db883b
Alexander Vasin <alexander.v.vasin@gmail.com>
2014-11-14 11:22:37 +0300

    Removed update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git rm index.html
-git commit -m "index.html removed by script"
-git checkout master
-./publish.sh
-git commit -a -m "Updated by script"
-git checkout gh-pages
-git add index.html
-git commit index.html -m "index.html added by script"
