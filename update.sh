870085d48f29f67889f4306203d4ef9684a1a27c
Alexander Vasin <alexander.v.vasin@gmail.com>
2014-11-14 11:21:25 +0300

    Added update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git checkout gh-pages
+git rm index.html
+git commit -m "index.html removed by script"
+git checkout master
+./publish.sh
+git commit -a -m "Updated by script"
+git checkout gh-pages
+git add index.html
+git commit index.html -m "index.html added by script"
