27fcf1512750f9c3c9736c2f8c8ea4e16be67c9e
Alexander Vasin <avasin@accesssoftek.com>
2019-08-30 02:45:53 +0300

    Moved scripts to a folder

--- scripts/publish_with_docker.sh
+++ scripts/publish_with_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./generator.py > Good-songs-to-play.ad
+./scripts/generator.py > Good-songs-to-play.ad
 sudo docker run --privileged=true -v $PWD:/documents asciidoctor/docker-asciidoctor asciidoctor -v -a stylesheet=stylesheets/minimal.css Good-songs-to-play.ad -o index.html
 sudo docker run --privileged=true -v $PWD:/documents asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-pdf -b pdf Good-songs-to-play.ad -o index.pdf
