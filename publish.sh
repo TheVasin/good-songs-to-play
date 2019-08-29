7269d96047eeeb897aabdde83aa4b1900293a399
Alexander Vasin <avasin@accesssoftek.com>
2019-08-30 02:43:53 +0300

    Moved scripts to a folder

--- publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./generator.py > Good-songs-to-play.ad
-asciidoctor -v -a stylesheet=stylesheets/minimal.css Good-songs-to-play.ad -o index.html
--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git rm index.html
 git rm index.pdf
 git commit -m "index.html and index.pdf removed by script"
 git checkout master
-./publish_with_docker.sh
+./scripts/publish_with_docker.sh
 git commit -a -m "Updated by script"
 git checkout gh-pages
 git add index.html
@@ -13,3 +13,4 @@ git add index.pdf
 git commit index.html -m "index.html added by script"
 git commit index.pdf -m "index.pdf added by script"
 git checkout master
+git push --all
