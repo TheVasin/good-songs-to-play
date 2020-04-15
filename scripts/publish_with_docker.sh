21368e4d872d417e78d388281d46b6092b1caefc
Alexander Vasin <avasin@accesssoftek.com>
2020-04-16 02:41:30 +0300

    Added and updated some songs for setlist

--- scripts/publish_with_docker.sh
+++ scripts/publish_with_docker.sh
@@ -2,3 +2,4 @@
 ./scripts/generator.py > Good-songs-to-play.ad
 sudo docker run --privileged=true -v $PWD:/documents asciidoctor/docker-asciidoctor asciidoctor -v -a stylesheet=stylesheets/minimal.css Good-songs-to-play.ad -o index.html
 sudo docker run --privileged=true -v $PWD:/documents asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-pdf -b pdf Good-songs-to-play.ad -o index.pdf
+sudo docker run --privileged=true -v $PWD:/documents asciidoctor/docker-asciidoctor asciidoctor -v -a stylesheet=stylesheets/minimal.css SetList.ad -o setlist.html
--- update.sh
+++ update.sh
@@ -3,14 +3,17 @@ git pull
 git checkout gh-pages
 git rm index.html
 git rm index.pdf
-git commit -m "index.html and index.pdf removed by script"
+git rm setlist.html
+git commit -m "index.html, setlist.html and index.pdf removed by script"
 git checkout master
 ./scripts/publish_with_docker.sh
 git commit -a -m "Updated by script"
 git checkout gh-pages
 git add index.html
 git add index.pdf
+git add setlist.html
 git commit index.html -m "index.html added by script"
 git commit index.pdf -m "index.pdf added by script"
+git commit setlist.html -m "setlist.html added by script"
 git checkout master
 git push --all
